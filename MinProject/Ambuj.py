330fc2894bf1cbf8ebe14527f40394d71ec7ce3e
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-02-22 14:12:10 +0530

    Update Ambuj.py

--- MinProject/Ambuj.py
+++ MinProject/Ambuj.py
@@ -1,22 +1,33 @@
-import zipfile 
-def crack(zfile,passw):
+import zipfile
+import os 
+def crack(zfile,passw,infol):
     try:
-        zfile.extractall(pwd=bytes(passw, 'utf-8'))
+        zfile.extractall(infol,pwd=bytes(passw, 'utf-8'))
         return(passw)
     except:
         return("0")
 
-print("Hello!!!!\n Welcome to ZipFile Cracker. Please Enter the file name.")
-filename=input()
-zfile=zipfile.ZipFile(filename)
-pfile=open('Passwords.txt')
-for i in pfile.readlines():
-    passw=i.strip('\n')
-    g=crack(zfile,passw)
-    if g!="0":
-        print("Password is: "+ g)
-        print("Contents of Zipfile: \n")
-        print(zfile.printdir())
-        break
-  
+ch='y'
+while ch=='y':
+    os.system('cls')
+    print("-------------------Hello!!!!Welcome to ZipFile Cracker--------------------------\nPlease Enter the zipfile file name.")
+    filename=input()    
+    if filename.endswith('.zip')==False: 
+        print("Please Enter a valid file type!!!") 
+    else:
+        zfile=zipfile.ZipFile(filename) 
+        print("Enter the folder where you want to extract the file")
+        infol=input()
+        pfile=open('Passwords.txt')
+        for i in pfile.readlines():
+            passw=i.strip('\n')
+            g=crack(zfile,passw,infol)
+            if g!="0":
+                print("Password is: "+ g)
+                print("Contents of Zipfile: \n")
+                print(zfile.printdir())
+                break
+    print("Do you want to continue(y/n): ") 
+    ch=input()
 zfile.close()   
+pfile.close()
